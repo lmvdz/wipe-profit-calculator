a5a0f1eb7d69f5e4d27613ec0a4d7286154ca367
unknown <lmvanderzande@gmail.com>
2021-11-13 17:23:34 -0600

    min/max and 2000 for 50k

--- calculate_profit.js
+++ calculate_profit.js
@@ -1,20 +1,63 @@
 const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 
-module.exports = async function calculate_profit(starting_wipe = 30895, number_of_nfts = 25, number_of_weeks = 25, starting_week = 1) {
-    const staking = 1.1
-    const current_price = (await CoinGeckoClient.coins.fetch('wipemyass', {})).data.market_data.current_price.usd
+module.exports = function calculate_profit_using_current_price(coin_id = 'wipemyass', starting_token = 30895, staking_reward = 1.1, staking_reward_weekly_interval = 1, number_of_nfts = 25, number_of_weeks = 25, starting_week = 1, denomination = 'usd') {
+    return new Promise(function(resolve, reject) {
+        let errors = []
+        if (starting_token < -1) {
+            errors.push('starting_token has to be greater than or equal to zero.')
+        }
+        if (number_of_nfts > 3000 || number_of_nfts < 0){
+            errors.push('number_of_nfts has to be greater than or equal to zero and less than three thousand.')
+        }
+        if (number_of_weeks > 25 || number_of_weeks < 1) {
+            errors.push('number_of_weeks has to be between 1 and 25 (inclusive).');
+        }
+        if (starting_week > 25 || starting_week < 1) {
+            errors.push('starting_week has to be between 1 and 25 (inclusive).')
+        }
+        if (errors.length > 0) {
+            errors.forEach(error => {
+                console.error(error);
+            })
+            reject(new Error('Failed to calculate the profit, check your parameters'));
+        }
+        
+        const weekly_token_from_nfts = () => {
+            return (3000 * number_of_nfts)
+        }
 
-    let current_wipe = 0;
-    current_wipe += starting_wipe;
-    
-    const weekly_wipe_from_nfts = () => {
-        return 3000 * number_of_nfts
-    }
-    
+        const weekly_token_50k = () => {
+            return (2000)
+        }
+        
+        let least_amount_of_wipe = 0 + starting_token;
+        for(let week_counter = starting_week; week_counter <= number_of_weeks; week_counter++) {
+            if (week_counter % staking_reward_weekly_interval === 0) {
+                least_amount_of_wipe *= (1 + (1 / staking_reward))
+            }
+            least_amount_of_wipe += weekly_token_from_nfts()
+            if (least_amount_of_wipe > 50000)
+                least_amount_of_wipe += weekly_token_50k()
+        }
+        let most_amount_of_wipe = 0 + starting_token;
+        for(let week_counter = starting_week; week_counter <= number_of_weeks; week_counter++) {
+            most_amount_of_wipe += weekly_token_from_nfts()
+            if (most_amount_of_wipe > 50000)
+                most_amount_of_wipe += weekly_token_50k()
+            if (week_counter % staking_reward_weekly_interval === 0) {
+                most_amount_of_wipe *= (1 + (1 / staking_reward))
+            }
+        }
 
-    for(let week_counter = starting_week; week_counter <= number_of_weeks; week_counter++) {
-        current_wipe = (staking * current_wipe) + weekly_wipe_from_nfts()
-    }
-    return Number(current_wipe * current_price).toFixed(2);
+        CoinGeckoClient.coins.fetch(coin_id, {}).then(response => {
+            const current_price = response.data.market_data.current_price[denomination]
+            resolve({ symbol: response.data.symbol, denomination: denomination, number_of_nfts: number_of_nfts, starting_token: starting_token, least_token: least_amount_of_wipe, most_token: most_amount_of_wipe, current_price: current_price, profit_most: Number(most_amount_of_wipe * current_price), profit_least: Number(least_amount_of_wipe * current_price), error: null});
+        }).catch(error => {
+            console.error(error);
+            resolve({ symbol: response.data.symbol, denomination: denomination, number_of_nfts: number_of_nfts, starting_token: starting_token, least_token: least_amount_of_wipe, most_token: most_amount_of_wipe, current_price: null, profit_most: null, error: new Error('Failed to retrieve current_price from coingecko')})
+        })
+        
+    })
+    
 }
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const calculate_profit = require('./calculate_profit.js');
-// calculate_profit(starting_wipe, number_of_nfts, number_of_weeks, starting_week)
-calculate_profit(30000, 25, 25, 2).then(profit => {
-  console.log(`$ ${profit}`);
-});
+const calculate_profit_using_current_price = require('./calculate_profit.js');
+
+calculate_profit_using_current_price('wipemyass', 393000, 10, 1, 107, 25, 2, 'usd').then(calculation => {
+  if (calculation.error) {
+    console.error(error);
+    console.log(`Total $${calculation.symbol.toUpperCase()}:\t ${calculation.total_token}`)
+  } else {
+    console.log(`Start $${calculation.symbol.toUpperCase()}:\t ${calculation.starting_token}`)
+    console.log(`NFT Count:\t ${calculation.number_of_nfts}`)
+    console.log(`Total $${calculation.symbol.toUpperCase()} (min - max):\t ${calculation.least_token} - ${calculation.most_token}`)
+    console.log(`Price (${calculation.denomination.toUpperCase()}):\t\t\t $ ${calculation.current_price}`)
+    console.log(`Total ${calculation.denomination.toUpperCase()} (min - max):\t\t $ ${calculation.profit_least} - ${calculation.profit_most}`)
+  }
+})
