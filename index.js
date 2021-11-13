a47f952939e4899d480c7dd16a77b668f7c2065d
unknown <lmvanderzande@gmail.com>
2021-11-13 10:56:40 -0600

    add profit_calculator script

---
+++ index.js
@@ -0,0 +1,21 @@
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
+
+export default async function calculate_profit(starting_wipe = 30895, number_of_nfts = 25, number_of_weeks = 24, starting_week = 1) {
+    const staking = 1.1
+    const current_price = (await CoinGeckoClient.coins.fetch('wipemyass', {})).data.market_data.current_price.usd
+
+    let current_wipe = 0;
+    current_wipe += starting_wipe;
+    
+    const weekly_wipe_from_nfts = () => {
+        return 3000 * number_of_nfts
+    }
+    
+
+    for(let week_counter = starting_week; week_counter <= number_of_weeks; week_counter++) {
+        current_wipe = (staking * current_wipe) + weekly_wipe_from_nfts()
+    }
+
+    console.log(`$ ${Number(current_wipe * current_price).toFixed(2)}`)
+}
