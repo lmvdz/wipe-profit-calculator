f5442952d4229d5bf5fec1f05c5e166b84f72990
unknown <lmvanderzande@gmail.com>
2021-11-13 11:02:48 -0600

    index.js example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const calculate_profit = require('./calculate_profit.js');
-calculate_profit(0, 25, 25, 1).then(profit => {
+// calculate_profit(starting_wipe, number_of_nfts, number_of_weeks, starting_week)
+calculate_profit(30000, 25, 25, 2).then(profit => {
   console.log(`$ ${profit}`);
 });
